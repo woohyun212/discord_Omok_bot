f14790b735fd3a6c6aeead6bc22a46c4c80d67df
woohyun212 <woohyun212@gmail.com>
2021-09-12 22:35:01 +0900

    오목봇 4일차
    o!start @상대 :게임시작
    o!ss @상대 돌위치 : 돌두기
    o!end @상대 : 게임종료
    o!help : 도움말

--- Othello.py
+++ Othello.py
@@ -1,23 +1,69 @@
 import numpy as np
-from PIL import Image, ImageDraw, ImageFilter
+from PIL import Image, ImageDraw
+import os.path
 
 
-class OthelloGame:
-    white = '○'
-    black = '●'
-    board = np.array([[int(i) for i in '0' * 19] for j in range(19)])
+class OngoingException(Exception):
+    def __init__(self, message):
+        self.message = message
+
+    def __str__(self):
+        return self.message
+
+
+class CannotSetStoneException(Exception):
+    def __init__(self, message):
+        self.message = message
+
+    def __str__(self):
+        return self.message
+
+
+def isSearched(str='_'):
+    filenames = os.listdir('game_saving')
+    for filename in filenames:
+        full_filename = os.path.join('game_saving', filename)
+        # print(1, str)
+        # print(2, full_filename)
+        if str in full_filename:
+            return True
+    return False
+
+
+class OmokGame:
+    black = '○'  # player1
+    white = '●'  # player2
 
     def __init__(self, player1, player2):
-        self.player1, self.player2 = player1, player2
+        self.player1, self.player2 = sorted([player1, player2])
+
+        if isSearched(f'{self.player1}vs{self.player2}.npy'):
+            print("진행 중인 게임이 존재합니다. 진행중이던 게임을 불러옵니다.")
+            self.board = self.getLoadGameData()
+            # P1vsP2 파일은 없는데, P1 or P2 가 검색되는 경우.
+            self.save_file_location = f'game_saving/{self.player1}vs{self.player2}.npy'
+
+        elif not isSearched(f'{self.player1}vs{self.player2}.npy') \
+                and (isSearched(str(self.player1)) or isSearched(str(self.player2))):
+            print("진행중인 게임이 있습니다! 먼저 게임을 종료해주세요!")
+            raise OngoingException('The game is Ongoing.')
+
+        else:
+            print("진행 중인 게임이 없습니다. 초기화 후 새로운 게임을 시작합니다.")
+            self.board = np.array([[int(i) for i in '0' * 19] for _ in range(19)])
+            self.gameSave()
+            self.save_file_location = f'game_saving/{self.player1}vs{self.player2}.npy'
 
     def __str__(self):
         return f'Player1 = {self.player1} [BLACK] \n' \
                f'Player2 = {self.player2} [WHITE]'
 
     def __del__(self):
-        print(f"{self.player1}와 {self.player2}의 게임이 종료되었어요!")
+        # np.save(f'game_saving/{self.player1}vs{self.player2}', self.board)
+        # self.gameSave()
+        pass
 
-    def getTextBoard(self):
+    def getStringFromBoard(self):
         board_str = list(map(int, self.board.flat))
         for index in enumerate(board_str):
             if board_str[index[0]] == 1:
@@ -34,7 +80,7 @@ class OthelloGame:
         board = set_style + line_head + ''.join(line_body) + end_style
         return board
 
-    def getImageBoard(self):
+    def getImageFromBoard(self):
         pixel_pos = {1: 52, 2: 102, 3: 151, 4: 200, 5: 249, 6: 298, 7: 347, 8: 396, 9: 445, 10: 500, 11: 554, 12: 603,
                      13: 652, 14: 701, 15: 750, 16: 799, 17: 848, 18: 898, 19: 947}
         img_board = Image.open('omokBoard.png')
@@ -56,27 +102,50 @@ class OthelloGame:
                                                           fill='white',
                                                           outline='black')
             # print()
-        img_board.show()
+        # img_board.show()
         return img_board
 
-    def gameStart(self):
+    def gameSave(self):
+        np.save(f'game_saving/{self.player1}vs{self.player2}', self.board)
+
+    def gameImageSave(self):
+        self.getImageFromBoard().save(f'game_saving/{self.player1}vs{self.player2}.png', 'png')  # 이미지 저장
+
+    def getLoadGameData(self):
+        return np.load(f'game_saving/{self.player1}vs{self.player2}.npy')
+
+    def gameNewStart(self):
         pass
 
-    def putStone(self, player, x, y):
-        if player == self.player1:
+    def gameTerminate(self):
+        os.remove(self.save_file_location)
+        print('세이브 파일 삭제됨')
+        pass
+
+    def putStone(self, req_user, position: str):
+        position = position.upper()
+        al_nu = dict(zip('ABCDEFGHIJKLMNOPQRS', map(int, '1 2 3 4 5 6 7 8 9 10 11 12 13 14 15 16 17 18 19'.split())))
+        x, y = al_nu[position[:1]], int(position[1:])
+        if self.board[x - 1, y - 1] != 0:
+            raise CannotSetStoneException("Cannot Set Stone at the position")
+        if req_user == self.player1:
+            # print(al_nu[position[:1]], position[1:], player)
             self.board[x - 1, y - 1] = 1
         else:
+            # print(al_nu[position[:1]], position[1:], player)
             self.board[x - 1, y - 1] = 2
 
 
 if __name__ == '__main__':
     pass
-    o = OthelloGame(1, 2)
-    o.gameStart()
-    o.putStone(1, 11, 11)
-    o.putStone(1, 12, 12)
-    o.putStone(1, 13, 13)
-    o.putStone(1, 14, 14)
-    o.putStone(2, 15, 15)
-    print(o.getImageBoard())
+    o = OmokGame(12345, 54321)
     del o
+    o = OmokGame(12345, 54321)
+    o.putStone(123, 'A1')
+    o.putStone(321, 'B4')
+    o.getImageFromBoard().show()
+    del o
+    o = OmokGame(12345, 54321)
+    o.gameTerminate()
+
+# del o
--- main.py
+++ main.py
@@ -1,8 +1,10 @@
 import discord
 from discord.ext import commands
 from Othello import *
+from io import BytesIO
+from discord.ext.commands import CommandNotFound
 
-app = commands.Bot(command_prefix='o!')
+app = commands.Bot(command_prefix='o!', help_command=None)
 
 
 @app.event
@@ -13,35 +15,94 @@ async def on_ready():
     await app.change_presence(status=discord.Status.online, activity=None)
 
 
+@app.event
+async def on_command_error(ctx, error):
+    if isinstance(error, CommandNotFound):
+        return await ctx.send("그런 명령어 없습니다~")
+    raise error
+
+
 @app.command()
 async def hello(ctx):
     await ctx.send('Hello, World!')
 
 
+# @app.command()
+# async def 임베드(ctx):
+#     _embed = discord.Embed(
+#         title='This is Embed',
+#         description='This is description of Embed',
+#         color=0x00ff00)
+#     _embed.add_field(
+#         name='This is feild 1',
+#         value='This is Value of Field 1',
+#         inline=True)
+#     await ctx.send(embed=_embed)
+
+
+def sendImage(game):
+    with BytesIO() as image_binary:
+        game.getImageFromBoard().save(image_binary, 'png')
+        image_binary.seek(0)
+        return discord.File(fp=image_binary, filename="image.png")
+
+
+def getDiscordId(name_tag):
+    return int(name_tag[3:21])
+
+
 @app.command()
-async def 임베드(ctx):
+async def start(ctx, player2):
+    try:
+        game = OmokGame(ctx.author.id, getDiscordId(player2))
+        game.gameSave()
+        out = sendImage(game)
+        await ctx.send(f"<@!{ctx.author.id}> vs {player2}\n", file=out)
+    except OngoingException:
+        _embed = discord.Embed(
+            title='게임을 진행중인 플레이어가 있습니다!\n먼저 진행중인 게임을 종료해주세요!',
+            color=0xff0000)
+        await ctx.send(embed=_embed)
+
+
+@app.command()
+async def ss(ctx, player2, position):
+    try:
+        game = OmokGame(ctx.author.id, getDiscordId(player2))
+        game.putStone(ctx.author.id, position)
+        out = sendImage(game)
+        game.gameSave()
+        await ctx.send(f"<@!{ctx.author.id}> vs {player2}\n", file=out)
+
+    except OngoingException:
+        _embed = discord.Embed(
+            title='진행중인 게임이 있습니다!\n먼저 진행중인 게임을 종료해주세요!',
+            color=0xff0000)
+        await ctx.send(embed=_embed)
+    except CannotSetStoneException:
+        _embed = discord.Embed(
+            title='해당 위치에는 돌을 놓을 수 없습니다 ^^',
+            color=0xff0000)
+        await ctx.send(embed=_embed)
+
+
+@app.command(name='게임 종료', description="")
+async def end(ctx, player2):
+    game = OmokGame(ctx.author.id, getDiscordId(player2))
+    game.gameTerminate()
     _embed = discord.Embed(
-        title='This is Embed',
-        description='This is description of Embed',
-        color=0x00ff00)
-    _embed.add_field(
-        name='This is feild 1',
-        value='This is Value of Field 1',
-        inline=True)
+        title='게임이 종료되었습니다.',
+        color=0xaa1faa)
     await ctx.send(embed=_embed)
 
 
-@app.command()
-async def 게임시작(ctx, player1, player2):
-    game = OthelloGame(player1, player2)
-    print(game.printBoard(),)
-    _embed = discord.Embed(
-        title=f'5Mok',
-        description=game.printBoard(),
-        color=0x00ff00)
-    await ctx.send(f"{player1} vs {player2}\n",embed=_embed)
-
-
+@app.command(name="help", description="Returns all commands available")
+async def help(ctx):
+    helptext = "```"
+    for command in app.commands:
+        helptext += f"{command}\n"
+    helptext += "```"
+    await ctx.send(helptext)
 
 
 app.run('ODg1MzYzNTUyNTk1MDg3NDEz.YTl9EA.3BIKD5J7cCjapiIcQb_hYJL8Ko8')
